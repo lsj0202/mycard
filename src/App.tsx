33f3288f893421bc5563d83485d44965e4cf2e29
lsj0202 <w1004123@gmail.com>
2023-11-27 01:09:30 +0900

    feat: add components Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Button from 'components/shared/Button'
 import Text from 'components/shared/Text'
 import './App.css'
 
@@ -5,6 +6,11 @@ function App() {
   return (
     <div>
       <Text typography="t1">t1</Text>
+      <Button color="success">이상진</Button>
+      <Button color="error">이상진</Button>
+      <Button color="success" weak={true} full={true}>
+        이상진
+      </Button>
     </div>
   )
 }
